81563c0be4d14beacfd7604dea5def5e7677d53b
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-03-31 23:14:19 +0200

    test Web Socket client

--- java/src/test/java/fr/xebia/xebay/client/BidderTest.java
+++ java/src/test/java/fr/xebia/xebay/client/BidderTest.java
@@ -1,5 +1,7 @@
 package fr.xebia.xebay.client;
 
+import fr.xebia.xebay.domain.BidOffer;
+import org.glassfish.jersey.client.HttpUrlConnectorProvider;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.junit.rules.ExternalResource;
 
@@ -14,18 +16,19 @@ public class BidderTest extends ExternalResource {
 
     protected RestBidder restBidder;
     protected WebTarget target;
+    BidOffer currentOffer;
 
     @Override
     protected void before() {
         String target = "http://localhost:8080/rest";
         this.restBidder = new RestBidder(target);
         try {
-            restBidder.getCurrentOffer();
+            currentOffer = restBidder.getCurrentOffer();
         } catch (ProcessingException e) {
             System.out.format("Please start a bid server on %s%n", target);
             throw e;
         }
-        this.target = ClientBuilder.newBuilder().register(JacksonFeature.class).build().target(target);
+        this.target = ClientBuilder.newBuilder().register(JacksonFeature.class).property(HttpUrlConnectorProvider.SET_METHOD_WORKAROUND, true).build().target(target);
     }
 
     protected String register(String name) {
@@ -44,4 +47,19 @@ public class BidderTest extends ExternalResource {
                 .delete();
     }
 
+    protected void bid(String apiKey) {
+        restBidder.bid(currentOffer.getItem().getName(), currentOffer.getItem().getValue() * 1.2, apiKey);
+    }
+
+    protected void activate(String pluginName) {
+        target.path("bidEngine/plugin").path(pluginName).path("activate").request()
+                .header(AUTHORIZATION, ADMIN_KEY)
+                .method("PATCH", Void.TYPE);
+    }
+
+    protected void deactivate(String pluginName) {
+        target.path("bidEngine/plugin").path(pluginName).path("deactivate").request()
+                .header(AUTHORIZATION, ADMIN_KEY)
+                .method("PATCH", Void.TYPE);
+    }
 }
--- java/src/test/java/fr/xebia/xebay/client/WebSocketBidderTest.java
+++ java/src/test/java/fr/xebia/xebay/client/WebSocketBidderTest.java
@@ -1,5 +1,6 @@
 package fr.xebia.xebay.client;
 
+import fr.xebia.xebay.domain.BidOffer;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -7,21 +8,87 @@ import javax.websocket.DeploymentException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class WebSocketBidderTest {
     @Rule
     public BidderTest bidderTest = new BidderTest();
 
     @Test
-    public void should_idontknow() throws DeploymentException, IOException, URISyntaxException, InterruptedException {
+    public void should_notify_bid_offer() throws DeploymentException, IOException, URISyntaxException, InterruptedException {
         String apiKey = null;
+        WebSocketBidder bidder = null;
         try {
             apiKey = bidderTest.register("email@provider.net");
+            bidder = new WebSocketBidder("ws://localhost:8080/socket/bidEngine/" + apiKey);
+            final WebSocketBidderStore bidOfferResult = new WebSocketBidderStore();
+            bidder.onBidOfferChange(updatedBidOffer -> {
+                bidOfferResult.set(updatedBidOffer);
+                synchronized (this) {
+                    this.notifyAll();
+                }
+            });
 
-            WebSocketBidder bidder = new WebSocketBidder("ws://localhost:8080/socket/bidEngine/" + apiKey, System.out::println);
+            bidderTest.bid(apiKey);
+
+            synchronized (this) {
+                this.wait(3000);
+            }
+            assertThat(bidOfferResult.updatedBidOffer).isNotNull();
+            assertThat(bidOfferResult.updatedBidOffer.getOwner()).isNull();
+            assertThat(bidOfferResult.updatedBidOffer.getItem().getName()).isNotEmpty();
         } finally {
+            if (bidder != null) {
+                bidder.session.close();
+            }
             if (apiKey != null) {
                 bidderTest.unregister(apiKey);
             }
+        }
+    }
+
+    @Test
+    public void should_notify_info() throws DeploymentException, IOException, URISyntaxException, InterruptedException {
+        String apiKey = null;
+        WebSocketBidder bidder = null;
+        try {
+            apiKey = bidderTest.register("email@provider.net");
+            bidder = new WebSocketBidder("ws://localhost:8080/socket/bidEngine/" + apiKey);
+            final WebSocketBidderStore result = new WebSocketBidderStore();
+            bidder.onInfo(info -> {
+                result.setInfo(info);
+                synchronized (this) {
+                    this.notifyAll();
+                }
+            });
+
+            bidderTest.activate("BankBuyEverything");
+
+            synchronized (this) {
+                this.wait(3000);
+            }
+            assertThat(result.info).isEqualTo("BankBuyEverything is now active");
+        } finally {
+            if (bidder != null) {
+                bidder.session.close();
+            }
+            bidderTest.deactivate("BankBuyEverything");
+            if (apiKey != null) {
+                bidderTest.unregister(apiKey);
+            }
+        }
+    }
+
+    private final static class WebSocketBidderStore {
+        private BidOffer updatedBidOffer;
+        private String info;
+
+        private void set(BidOffer updatedBidOffer) {
+            this.updatedBidOffer = updatedBidOffer;
+        }
+
+        private void setInfo(String info) {
+            this.info = info;
         }
     }
 }
